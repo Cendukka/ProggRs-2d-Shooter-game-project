2e4b08f8e98451f29de3aa8bb2275606910739eb
Samuli LEhtonen <samuilehtonen33@gmail.com>
2020-02-02 04:25:42 +0200

    fix to commit:"Startbutton, space ship and movement"
    Startscene's button works now

--- src/StartScene.cpp
+++ src/StartScene.cpp
@@ -45,6 +45,31 @@ void StartScene::handleEvents()
 		case SDL_QUIT:
 			TheGame::Instance()->quit();
 			break;
+		case SDL_MOUSEMOTION:
+			m_mousePosition.x = event.motion.x;
+			m_mousePosition.y = event.motion.y;
+
+			/*std::cout << "Mouse X: " << m_mousePosition.x << std::endl;
+			std::cout << "Mouse Y: " << m_mousePosition.y << std::endl;
+			std::cout << "---------------------------------------------" << std::endl;*/
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			switch (event.button.button)
+			{
+			case SDL_BUTTON_LEFT:
+				m_pStartButton->setMouseButtonClicked(true);
+				break;
+			}
+
+			break;
+		case SDL_MOUSEBUTTONUP:
+			switch (event.button.button)
+			{
+			case SDL_BUTTON_LEFT:
+				m_pStartButton->setMouseButtonClicked(false);
+				break;
+			}
+			break;
 		case SDL_KEYDOWN:
 			switch (event.key.keysym.sym)
 			{
